052f20daabc6ad2926a14758a0a479c24a47de8f
justinkhull <hullj1@g.cofc.edu>
2017-10-07 18:03:16 -0400

    completed randompic

---
+++ randomPic/randomPicJS.js
@@ -0,0 +1,4 @@
+
+var images = ["images/dog1.jpeg", "images/dog2.jpg", "images/dog3.jpg", "images/dog4.jpeg", "images/dog5.jpg"];
+var imageNumber = Math.floor(Math.random() * images.length);//("Enter a number from 0 to 4", "");
+document.images[0].src = images[imageNumber];
